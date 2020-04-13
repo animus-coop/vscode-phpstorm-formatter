ddf696cc9d3dfc4f893c436029007d739575d2cd
Manuel Valle <manu@animus.com.ar>
2020-04-13 14:56:05 -0300

    Renamed 0.0.4-r1 to 0.0.5

--- built/Provider.js
+++ built/Provider.js
@@ -7,7 +7,33 @@ class Provider {
         this.formatter = new Formatter_1.default();
     }
     documentFormattingEditProvider() {
-        return vscode_1.languages.registerDocumentFormattingEditProvider({ scheme: 'file' }, {
+        console.log(vscode_1.languages);
+        return vscode_1.languages.registerDocumentFormattingEditProvider([
+            { scheme: 'file', language: 'php' },
+            { scheme: 'file', language: 'json' },
+            { scheme: 'file', language: 'python' },
+            { scheme: 'file', language: 'typescript' },
+            { scheme: 'file', language: 'xml' },
+            { scheme: 'file', language: 'xsl' },
+            { scheme: 'file', language: 'yaml' },
+            { scheme: 'file', language: 'vb' },
+            { scheme: 'file', language: 'javascript' },
+            { scheme: 'file', language: 'html' },
+            { scheme: 'file', language: 'ini' },
+            { scheme: 'file', language: 'java' },
+            { scheme: 'file', language: 'go' },
+            { scheme: 'file', language: 'css' },
+            { scheme: 'file', language: 'dockerfile' },
+            { scheme: 'file', language: 'cpp' },
+            { scheme: 'file', language: 'c' },
+            { scheme: 'file', language: 'less' },
+            { scheme: 'file', language: 'scss' },
+            { scheme: 'file', language: 'shellscript' },
+            { scheme: 'file', language: 'jsonc' },
+            { scheme: 'file', language: 'swift' },
+            { scheme: 'file', language: 'ruby' },
+            { scheme: 'file', language: 'sql' },
+        ], {
             provideDocumentFormattingEdits: (document) => {
                 return vscode_1.window.withProgress({
                     location: vscode_1.ProgressLocation.Notification,
